0949591f7e4ce87cd225b49f0f0cd192a06d7364
sorych <dmytro.sorych@osi.ca.gov>
2018-10-22 18:40:52 -0700

    COG-763 logging is improved in CAP-users-job

--- jobs-cap-users/src/main/java/gov/ca/cwds/jobs/cap/users/service/CapChangedUsersService.java
+++ jobs-cap-users/src/main/java/gov/ca/cwds/jobs/cap/users/service/CapChangedUsersService.java
@@ -9,8 +9,12 @@ import gov.ca.cwds.jobs.common.savepoint.LocalDateTimeSavePointService;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CapChangedUsersService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CapChangedUsersService.class);
 
   @Inject
   private LocalDateTimeSavePointService savePointService;
@@ -21,9 +25,10 @@ public class CapChangedUsersService {
   public List<ChangedUserDto> getCapChanges() {
     LocalDateTime savePointTime = savePointService.loadSavePoint().getTimestamp();
     List<UserAndOperation> userAndOperations = idmService.getCapChanges(savePointTime);
+    LOGGER.info("the number of CAP changes recieved is: {}", userAndOperations.size());
     return userAndOperations.stream()
-            .map(e -> new ChangedUserDto(e.getUser(), transformOperation(e.getOperation())))
-            .collect(Collectors.toList());
+        .map(e -> new ChangedUserDto(e.getUser(), transformOperation(e.getOperation())))
+        .collect(Collectors.toList());
   }
 
   private RecordChangeOperation transformOperation(OperationType operation) {
