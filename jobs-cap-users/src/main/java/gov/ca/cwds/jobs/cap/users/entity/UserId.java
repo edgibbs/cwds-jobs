07aea64e7ffd65d1c3d0a4703d2c0ffb677da45a
sorych <dmytro.sorych@osi.ca.gov>
2018-10-30 14:28:07 -0700

    performanceTestMode introduced to get all RACFIDS from CWSCMS

--- jobs-cap-users/src/main/java/gov/ca/cwds/jobs/cap/users/entity/UserId.java
+++ jobs-cap-users/src/main/java/gov/ca/cwds/jobs/cap/users/entity/UserId.java
@@ -1,30 +1,40 @@
 package gov.ca.cwds.jobs.cap.users.entity;
 
 import gov.ca.cwds.data.legacy.cms.CmsPersistentObject;
-
+import java.time.LocalTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-@NamedQuery(
+@NamedQueries(
+    {@NamedQuery(
         name = UserId.CWSCMS_INCREMENTAL_LOAD_QUERY_NAME,
-        query = UserId.CWS_CMS_INCREMENTAL_LOAD_QUERY
-)
+        query = UserId.CWS_CMS_INCREMENTAL_LOAD_QUERY),
+        @NamedQuery(
+            name = UserId.CWSCMS_All_RACFIDS_QUERY_NAME,
+            query = UserId.CWS_CMS_ALL_RACFIDS_QUERY)
+    })
+
 @Entity
 @Table(name = "USERID_T")
 public class UserId extends CmsPersistentObject {
 
   static final String CWS_CMS_INCREMENTAL_LOAD_QUERY =
-          "select distinct u.logonId from UserId u left join StaffPerson s on u.staffPersonId = s.id " +
-                  "left join CwsOffice o on s.cwsOffice = o.officeId " +
-                  " where u.lastUpdatedTime > :dateAfter or s.lastUpdatedTime > :dateAfter or o.lastUpdatedTime > :dateAfter";
+      "select distinct u.logonId from UserId u left join StaffPerson s on u.staffPersonId = s.id " +
+          "left join CwsOffice o on s.cwsOffice = o.officeId " +
+          " where u.lastUpdatedTime > :dateAfter or s.lastUpdatedTime > :dateAfter or o.lastUpdatedTime > :dateAfter";
+
+  static final String CWS_CMS_ALL_RACFIDS_QUERY =
+      "select distinct u.logonId from UserId u where u.endTime is null";
 
   private static final long serialVersionUID = 2128876585165704533L;
 
   public static final String CWSCMS_INCREMENTAL_LOAD_QUERY_NAME = "UserId.cwscmsIncrementalLoadQuery";
 
+  public static final String CWSCMS_All_RACFIDS_QUERY_NAME = "UserId.cwscmsAllRacfIds";
 
   @Id
   @Column(name = "IDENTIFIER")
@@ -35,6 +45,9 @@ public class UserId extends CmsPersistentObject {
 
   @Column(name = "LOGON_ID")
   private String logonId;
+
+  @Column(name = "END_TM")
+  private LocalTime endTime;
 
   public String getId() {
     return id;
@@ -58,6 +71,14 @@ public class UserId extends CmsPersistentObject {
 
   public void setLogonId(String logonId) {
     this.logonId = logonId;
+  }
+
+  public LocalTime getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(LocalTime endTime) {
+    this.endTime = endTime;
   }
 
   @Override
