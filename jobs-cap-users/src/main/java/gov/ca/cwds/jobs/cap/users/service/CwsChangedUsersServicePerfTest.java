6f5b7e90ed7527e2b913610fccfc37f4dd351136
sorych <dmytro.sorych@osi.ca.gov>
2018-10-30 15:58:26 -0700

    sonar is fixed

--- jobs-cap-users/src/main/java/gov/ca/cwds/jobs/cap/users/service/CwsChangedUsersServicePerfTest.java
+++ jobs-cap-users/src/main/java/gov/ca/cwds/jobs/cap/users/service/CwsChangedUsersServicePerfTest.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 
 public class CwsChangedUsersServicePerfTest implements CwsChangedUsersService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(CwsChangedUsersServiceImpl.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CwsChangedUsersServicePerfTest.class);
 
   @Inject
   private IdmService idmService;
