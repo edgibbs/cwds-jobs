f14f9b7a390856b29c9b4622d00cfec37b03490f
Alexander Serbin <alserbin@gmail.com>
2019-04-09 11:39:22 -0700

    SEAR-571 Addressing hanging problem

--- jobs-common/src/main/java/gov/ca/cwds/jobs/common/inject/DataAccessModule.java
+++ jobs-common/src/main/java/gov/ca/cwds/jobs/common/inject/DataAccessModule.java
@@ -1,16 +1,19 @@
 package gov.ca.cwds.jobs.common.inject;
 
 import com.google.inject.AbstractModule;
-import gov.ca.cwds.jobs.common.exception.JobsException;
 import gov.ca.cwds.jobs.common.util.SessionFactoryUtil;
 import io.dropwizard.db.DataSourceFactory;
 import java.util.List;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Alexander Serbin on 3/18/2019
  */
 public abstract class DataAccessModule extends AbstractModule {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DataAccessModule.class);
 
   private SessionFactory sessionFactory;
 
@@ -20,12 +23,15 @@ public abstract class DataAccessModule extends AbstractModule {
       sessionFactory = SessionFactoryUtil
           .buildSessionFactory(dataSourceFactory, dataSourceName, entityClasses);
     } catch (Exception e) {
-      throw new JobsException(
-          String.format("Couldn't build session factory %s", dataSourceName), e);
+      LOGGER.error(String.format("Couldn't build session factory %s", dataSourceName), e);
+      //This is workaround to address hanging. Temporary solution that needs to be replaced
+      //by permanent one
+      System.exit(1);
     }
   }
 
   protected SessionFactory getSessionFactory() {
     return sessionFactory;
   }
+
 }
