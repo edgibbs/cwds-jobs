a017a9970d937f0519904e2370da372299bbac0e
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2019-04-17 11:41:10 -0700

    COG-1404: additional DB2 driver setting are applied to the cap-users job

--- jobs-cap-users/cap_users_index.bat
+++ jobs-cap-users/cap_users_index.bat
@@ -1,6 +1,6 @@
 @echo off
 rem call gradle clean shadowJar
 rem call mkdir cap-out
-java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cap-users-job-1.3-SNAPSHOT.jar ^
+java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cap-users-job-1.4-SNAPSHOT.jar ^
      -c config/unix_cap-users-job.yaml -l ./cap-out/ ^
      > ./cap-out/out_cap_users.txt 2>&1
