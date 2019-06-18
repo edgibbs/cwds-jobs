21554566ab44135ae04e1e76a25d5eb3a7b85c61
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2019-06-18 15:42:25 -0700

    COG-1445: Test for fully populated user is added

--- jobs-cap-users/src/test/java/gov/ca/cwds/jobs/cap/users/report/UsersReportBuilderTest.java
+++ jobs-cap-users/src/test/java/gov/ca/cwds/jobs/cap/users/report/UsersReportBuilderTest.java
@@ -1,15 +1,24 @@
 package gov.ca.cwds.jobs.cap.users.report;
 
+import static gov.ca.cwds.config.api.idm.Roles.CWS_WORKER;
+import static gov.ca.cwds.util.Utils.toSet;
 import static org.junit.Assert.assertEquals;
 
 import gov.ca.cwds.idm.dto.User;
+import org.junit.Before;
 import org.junit.Test;
 
 public class UsersReportBuilderTest {
 
+  private UsersReportBuilder usersReportBuilder;
+
+  @Before
+  public void before() {
+    usersReportBuilder = new UsersReportBuilder();
+  }
+
   @Test
   public void testBuildHeader() {
-    UsersReportBuilder usersReportBuilder = new UsersReportBuilder();
     assertEquals(
         "Name, Role, Permissions, County, RACFID, Email, Status, Enabled\n",
         usersReportBuilder.buildHeader());
@@ -17,7 +26,25 @@ public class UsersReportBuilderTest {
 
   @Test
   public void testEmptyRow() {
-    UsersReportBuilder usersReportBuilder = new UsersReportBuilder();
     assertEquals(" , , , , , , , \n", usersReportBuilder.buildRow(new User()));
+  }
+
+  @Test
+  public void testFullRow() {
+    User user = new User();
+    user.setFirstName("FirstName");
+    user.setLastName("LastName");
+    user.setRoles(toSet(CWS_WORKER));
+    user.setPermissions(toSet("Snapshot-rollout", "Facility-search-rollout"));
+    user.setCountyName("Madera");
+    user.setRacfid("RACFID");
+    user.setEmail("some.email@gmail.com");
+    user.setStatus("CONFIRMED");
+    user.setEnabled(Boolean.TRUE);
+
+    assertEquals(
+        "FirstName LastName, CWS-worker, Snapshot-rollout:Facility-search-rollout, Madera, "
+            + "RACFID, some.email@gmail.com, CONFIRMED, true\n",
+        usersReportBuilder.buildRow(user));
   }
 }
