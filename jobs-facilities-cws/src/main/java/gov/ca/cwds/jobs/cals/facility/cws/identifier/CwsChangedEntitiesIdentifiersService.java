0c16671b8835292415c0dfd7538e1171f4551ee6
David R. Smith <David.Smith@osi.ca.gov>
2020-04-01 09:05:16 -0700

    CMO-475: discrepancy between CWS/CMS and CARES Facility Search

--- jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/identifier/CwsChangedEntitiesIdentifiersService.java
+++ jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/identifier/CwsChangedEntitiesIdentifiersService.java
@@ -2,17 +2,19 @@ package gov.ca.cwds.jobs.cals.facility.cws.identifier;
 
 import static gov.ca.cwds.cals.Constants.UnitOfWork.CMS;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import com.google.inject.Inject;
+
 import gov.ca.cwds.jobs.cals.facility.cws.dao.CwsChangedIdentifierDao;
 import gov.ca.cwds.jobs.common.identifier.ChangedEntitiesIdentifiersService;
 import gov.ca.cwds.jobs.common.identifier.ChangedEntityIdentifier;
 import gov.ca.cwds.jobs.common.savepoint.LocalDateTimeSavePoint;
 import gov.ca.cwds.jobs.common.savepoint.TimestampSavePoint;
 import io.dropwizard.hibernate.UnitOfWork;
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Created by Alexander Serbin on 3/6/2018.
@@ -20,16 +22,15 @@ import java.util.Optional;
 public class CwsChangedEntitiesIdentifiersService
     implements ChangedEntitiesIdentifiersService<LocalDateTime> {
 
-  @Inject private CwsChangedIdentifierDao dao;
+  @Inject
+  private CwsChangedIdentifierDao dao;
 
   @Override
   @UnitOfWork(CMS)
   public Optional<TimestampSavePoint<LocalDateTime>> getNextSavePoint(
       TimestampSavePoint<LocalDateTime> previousSavePoint) {
-    return dao.getNextSavePoint(previousSavePoint.getTimestamp()).map(
-        LocalDateTimeSavePoint::new);
+    return dao.getNextSavePoint(previousSavePoint.getTimestamp()).map(LocalDateTimeSavePoint::new);
   }
-
 
   @Override
   @UnitOfWork(CMS)
