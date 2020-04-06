da7843b1d2bc2b5f2d785cfdeeaee2b0dc2bd47e
David R. Smith <David.Smith@osi.ca.gov>
2020-04-06 11:19:06 -0700

    CMO-475: discrepancy between CWS/CMS and CARES Facility Search

--- jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/QueryConstants.java
+++ jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/QueryConstants.java
@@ -1,7 +1,5 @@
 package gov.ca.cwds.jobs.cals.facility.cws;
 
-import gov.ca.cwds.jobs.common.RecordChangeOperation;
-
 /**
  * Created by Alexander Serbin on 12/3/2018
  */
@@ -51,17 +49,69 @@ public final class QueryConstants {
         + "FOR READ ONLY WITH UR";
     //@formatter:on
 
+    //@formatter:off
     public static final String GET_IDENTIFIERS_BETWEEN_TIMESTAMPS_QUERY =
-        "select new CwsChangedIdentifier(home.identifier, " + InitialMode.TIMESTAMP_FIELD_NAME
-            + ") " + SHARED_PART + AND + InitialMode.TIMESTAMP_FIELD_NAME + " > :" + DATE_AFTER
-            + AND + InitialMode.TIMESTAMP_FIELD_NAME + " < :" + DATE_BEFORE
-            + " and home.recordChangeOperation != '" + RecordChangeOperation.D.name() + "'"
-            + ORDER_BY + InitialMode.TIMESTAMP_FIELD_NAME + ", " + HOME_IDENTIFIER_FIELD_NAME;
+        "WITH STEP1 AS (\n"
+      + " SELECT\n"
+      + "   plh.IBMSNAP_LOGMARKER AS PLH_TS,\n"
+      + "   cst.IBMSNAP_LOGMARKER AS CST_TS,\n"
+      + "   stf.IBMSNAP_LOGMARKER AS STF_TS,\n"
+      + "   plh.IDENTIFIER\n"
+      + " FROM      {h-schema}PLC_HM_T plh\n"
+      + " LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
+      + " LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
+      + " WHERE plh.LICENSR_CD <> 'CL' "
+      + "   AND plh.PLC_FCLC <> 1420\n"
+      + "   AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
+      + "      AND (\n"
+      + "          plh.IBMSNAP_LOGMARKER BETWEEN :dateBefore AND :dateAfter\n"
+      + "       OR cst.IBMSNAP_LOGMARKER BETWEEN :dateBefore AND :dateAfter\n"
+      + "       OR stf.IBMSNAP_LOGMARKER BETWEEN :dateBefore AND :dateAfter\n"
+      + "    )\n"
+      + " ORDER BY plh.IBMSNAP_LOGMARKER, cst.IBMSNAP_LOGMARKER, stf.IBMSNAP_LOGMARKER, plh.IDENTIFIER\n"
+      + " FETCH FIRST BATCH_SIZE ROWS ONLY\n"
+      + ")\n"
+      + "SELECT MAX(x.LAST_TS) FROM (\n"
+      + "  SELECT MAX(s1.PLH_TS) AS LAST_TS FROM STEP1 s1\n"
+      + "  UNION ALL\n"
+      + "  SELECT MAX(s1.CST_TS) AS LAST_TS FROM STEP1 s1 WHERE s1.CST_TS IS NOT NULL\n"
+      + "  UNION ALL\n"
+      + "  SELECT MAX(s1.STF_TS) AS LAST_TS FROM STEP1 s1 WHERE s1.STF_TS IS NOT NULL\n"
+      + ") x\n"
+      + "FOR READ ONLY WITH UR";
+    //@formatter:on
 
+    //@formatter:off
     public static final String GET_NEXT_SAVEPOINT_QUERY =
-        "select " + InitialMode.TIMESTAMP_FIELD_NAME + SHARED_PART + AND
-            + InitialMode.TIMESTAMP_FIELD_NAME + " > :" + DATE_AFTER + ORDER_BY
-            + InitialMode.TIMESTAMP_FIELD_NAME + ", " + HOME_IDENTIFIER_FIELD_NAME;
+          "WITH STEP1 AS (\n"
+        + " SELECT x.IBMSNAP_LOGMARKER, 'X' AS IBMSNAP_OPERATION\n"
+        + " FROM (\n"
+        + "     SELECT MAX(plh.IBMSNAP_LOGMARKER) AS IBMSNAP_LOGMARKER\n"
+        + "     FROM {h-schema}PLC_HM_T plh\n"
+        + "     WHERE plh.LICENSR_CD <> 'CL' "
+        + "       AND plh.PLC_FCLC <> 1420\n"
+        + "       AND s1.IBMSNAP_OPERATION IN ('I','U')\n"
+        + "       AND plh.IBMSNAP_LOGMARKER > :dateAfter\n"
+        + " ) x\n"
+        + " UNION ALL\n"
+        + " SELECT cst.IBMSNAP_LOGMARKER, cst.IBMSNAP_OPERATION\n"
+        + " FROM {h-schema}CNTY_CST cst\n"
+        + " WHERE cst.IBMSNAP_LOGMARKER > :dateAfter\n"
+        + " UNION ALL\n"
+        + " SELECT stf.IBMSNAP_LOGMARKER, stf.IBMSNAP_OPERATION\n"
+        + " FROM      {h-schema}CNTY_CST cst\n"
+        + " LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
+        + " WHERE stf.IBMSNAP_LOGMARKER > :dateAfter\n"
+        + "), STEP2 AS (\n"
+        + "  SELECT s1.IBMSNAP_LOGMARKER, s1.IBMSNAP_OPERATION\n"
+        + "  FROM STEP1 s1\n"
+        + "  WHERE s1.IBMSNAP_OPERATION IN ('X','I','U')\n"
+        + "    AND s1.IBMSNAP_LOGMARKER > :dateAfter\n"
+        + ")\n"
+        + "SELECT MAX(s2.IBMSNAP_LOGMARKER) AS MAX_LST_UPD_TS\n"
+        + "FROM STEP2 s2\n"
+        + "FOR READ ONLY WITH UR";
+    //@formatter:on
 
     private InitialMode() {
       // utility class
@@ -160,7 +210,8 @@ public final class QueryConstants {
         + " FROM (\n"
         + "     SELECT MAX(plh.IBMSNAP_LOGMARKER) AS IBMSNAP_LOGMARKER\n"
         + "     FROM {h-schema}PLC_HM_T plh\n"
-        + "     WHERE plh.LICENSR_CD <> 'CL' AND plh.PLC_FCLC <> 1420\n"
+        + "     WHERE plh.LICENSR_CD <> 'CL' "
+        + "       AND plh.PLC_FCLC <> 1420\n"
         + "       AND plh.IBMSNAP_LOGMARKER > :dateAfter\n"
         + " ) x\n"
         + " UNION ALL\n"
@@ -175,7 +226,7 @@ public final class QueryConstants {
         + "), STEP2 AS (\n"
         + "  SELECT s1.IBMSNAP_LOGMARKER, s1.IBMSNAP_OPERATION\n"
         + "  FROM STEP1 s1\n"
-        + "  WHERE s1.IBMSNAP_OPERATION IN ('X','I','U')\n"
+        + "  WHERE s1.IBMSNAP_OPERATION IN ('X','I','U','D')\n"
         + "    AND s1.IBMSNAP_LOGMARKER > :dateAfter\n"
         + ")\n"
         + "SELECT MAX(s2.IBMSNAP_LOGMARKER) AS MAX_LST_UPD_TS\n"
