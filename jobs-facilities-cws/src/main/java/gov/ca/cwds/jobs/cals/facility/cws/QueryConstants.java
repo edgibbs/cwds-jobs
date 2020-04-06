02e285b14f532a7492d08593b3a56a9e881a43db
David R. Smith <David.Smith@osi.ca.gov>
2020-04-06 14:27:48 -0700

    CMO-475: discrepancy between CWS/CMS and CARES Facility Search

--- jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/QueryConstants.java
+++ jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/QueryConstants.java
@@ -26,13 +26,9 @@ public final class QueryConstants {
 
   public static class InitialMode {
 
-    @SuppressWarnings("squid:S3008") // the name TIMESTAMP_FIELD_NAME matches the regular expression
-                                     // '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'
-    private static final String TIMESTAMP_FIELD_NAME = "home.lastUpdatedTime";
-
     //@formatter:off
     public static final String GET_IDENTIFIERS_AFTER_TIMESTAMP_QUERY =
-          "SELECT plh.IDENTIFIER, plh.IBMSNAP_OPERATION, plh.IBMSNAP_LOGMARKER\n"
+          "SELECT plh.IDENTIFIER, plh.IBMSNAP_OPERATION, plh.LST_UPD_TS AS IBMSNAP_LOGMARKER\n"
         + "FROM      {h-schema}PLC_HM_T plh\n"
         + "LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
         + "LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
@@ -40,9 +36,9 @@ public final class QueryConstants {
         + "  AND plh.PLC_FCLC   <> 1420 "
         + "  AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
         + "  AND (\n"
-        + "      plh.IBMSNAP_LOGMARKER > :dateAfter\n"
-        + "   OR cst.IBMSNAP_LOGMARKER > :dateAfter\n"
-        + "   OR stf.IBMSNAP_LOGMARKER > :dateAfter\n"
+        + "      plh.LST_UPD_TS > :dateAfter\n"
+        + "   OR cst.LST_UPD_TS > :dateAfter\n"
+        + "   OR stf.LST_UPD_TS > :dateAfter\n"
         + ")\n"
         + "ORDER BY plh.IBMSNAP_LOGMARKER, cst.IBMSNAP_LOGMARKER, stf.IBMSNAP_LOGMARKER, plh.IDENTIFIER\n"
         + "FETCH FIRST BATCH_SIZE ROWS ONLY\n"
@@ -53,9 +49,9 @@ public final class QueryConstants {
     public static final String GET_IDENTIFIERS_BETWEEN_TIMESTAMPS_QUERY =
         "WITH STEP1 AS (\n"
       + " SELECT\n"
-      + "   plh.IBMSNAP_LOGMARKER AS PLH_TS,\n"
-      + "   cst.IBMSNAP_LOGMARKER AS CST_TS,\n"
-      + "   stf.IBMSNAP_LOGMARKER AS STF_TS,\n"
+      + "   plh.LST_UPD_TS AS PLH_TS,\n"
+      + "   cst.LST_UPD_TS AS CST_TS,\n"
+      + "   stf.LST_UPD_TS AS STF_TS,\n"
       + "   plh.IDENTIFIER\n"
       + " FROM      {h-schema}PLC_HM_T plh\n"
       + " LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
@@ -64,11 +60,11 @@ public final class QueryConstants {
       + "   AND plh.PLC_FCLC   <> 1420\n"
       + "   AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
       + "      AND (\n"
-      + "          plh.IBMSNAP_LOGMARKER BETWEEN :dateBefore AND :dateAfter\n"
-      + "       OR cst.IBMSNAP_LOGMARKER BETWEEN :dateBefore AND :dateAfter\n"
-      + "       OR stf.IBMSNAP_LOGMARKER BETWEEN :dateBefore AND :dateAfter\n"
+      + "          plh.LST_UPD_TS BETWEEN :dateBefore AND :dateAfter\n"
+      + "       OR cst.LST_UPD_TS BETWEEN :dateBefore AND :dateAfter\n"
+      + "       OR stf.LST_UPD_TS BETWEEN :dateBefore AND :dateAfter\n"
       + "    )\n"
-      + " ORDER BY plh.IBMSNAP_LOGMARKER, cst.IBMSNAP_LOGMARKER, stf.IBMSNAP_LOGMARKER, plh.IDENTIFIER\n"
+      + " ORDER BY plh.LST_UPD_TS, cst.LST_UPD_TS, stf.LST_UPD_TS, plh.IDENTIFIER\n"
       + " FETCH FIRST BATCH_SIZE ROWS ONLY\n"
       + ")\n"
       + "SELECT MAX(x.LAST_TS) FROM (\n"
@@ -85,9 +81,9 @@ public final class QueryConstants {
     public static final String GET_FIRST_TS_AFTER_SAVEPOINT_QUERY =
           "WITH STEP1 AS (\n"
         + " SELECT\n"
-        + "   plh.IBMSNAP_LOGMARKER AS PLH_TS,\n"
-        + "   cst.IBMSNAP_LOGMARKER AS CST_TS,\n"
-        + "   stf.IBMSNAP_LOGMARKER AS STF_TS,\n"
+        + "   plh.LST_UPD_TS AS PLH_TS,\n"
+        + "   cst.LST_UPD_TS AS CST_TS,\n"
+        + "   stf.LST_UPD_TS AS STF_TS,\n"
         + "   plh.IDENTIFIER\n"
         + " FROM      {h-schema}PLC_HM_T plh\n"
         + " LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
@@ -118,7 +114,7 @@ public final class QueryConstants {
           "WITH STEP1 AS (\n"
         + " SELECT x.IBMSNAP_LOGMARKER, 'X' AS IBMSNAP_OPERATION\n"
         + " FROM (\n"
-        + "     SELECT MAX(plh.IBMSNAP_LOGMARKER) AS IBMSNAP_LOGMARKER\n"
+        + "     SELECT MAX(plh.LST_UPD_TS) AS IBMSNAP_LOGMARKER\n"
         + "     FROM {h-schema}PLC_HM_T plh\n"
         + "     WHERE plh.LICENSR_CD <> 'CL' "
         + "       AND plh.PLC_FCLC   <> 1420\n"
@@ -126,11 +122,11 @@ public final class QueryConstants {
         + "       AND plh.IBMSNAP_LOGMARKER > :dateAfter\n"
         + " ) x\n"
         + " UNION ALL\n"
-        + " SELECT cst.IBMSNAP_LOGMARKER, cst.IBMSNAP_OPERATION\n"
+        + " SELECT cst.LST_UPD_TS AS IBMSNAP_LOGMARKER, cst.IBMSNAP_OPERATION\n"
         + " FROM {h-schema}CNTY_CST cst\n"
         + " WHERE cst.IBMSNAP_LOGMARKER > :dateAfter\n"
         + " UNION ALL\n"
-        + " SELECT stf.IBMSNAP_LOGMARKER, stf.IBMSNAP_OPERATION\n"
+        + " SELECT stf.LST_UPD_TS AS IBMSNAP_LOGMARKER, stf.IBMSNAP_OPERATION\n"
         + " FROM      {h-schema}CNTY_CST cst\n"
         + " LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
         + " WHERE stf.IBMSNAP_LOGMARKER > :dateAfter\n"
@@ -151,10 +147,6 @@ public final class QueryConstants {
   }
 
   public static class IncrementalMode {
-
-    @SuppressWarnings("squid:S3008") // the name TIMESTAMP_FIELD_NAME matches the regular expression
-                                     // '^[A-Z][A-Z0-9]*(_[A-Z0-9]+)*$'
-    private static final String TIMESTAMP_FIELD_NAME = "home.replicationLastUpdated";
 
     //@formatter:off
     public static final String GET_IDENTIFIERS_AFTER_TIMESTAMP_QUERY =
