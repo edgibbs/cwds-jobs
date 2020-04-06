385ff309d919d9198b17439dc21a27eaf378da57
David R. Smith <David.Smith@osi.ca.gov>
2020-04-06 11:35:14 -0700

    CMO-475: discrepancy between CWS/CMS and CARES Facility Search

--- jobs-facilities-common/src/main/java/gov/ca/cwds/jobs/cals/facility/AbstractChangedFacilityService.java
+++ jobs-facilities-common/src/main/java/gov/ca/cwds/jobs/cals/facility/AbstractChangedFacilityService.java
@@ -1,16 +1,17 @@
 package gov.ca.cwds.jobs.cals.facility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gov.ca.cwds.cals.service.dto.FacilityDto;
 import gov.ca.cwds.jobs.common.entity.ChangedEntityService;
 import gov.ca.cwds.jobs.common.identifier.ChangedEntityIdentifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by Alexander Serbin on 3/28/2018.
  */
-public abstract class AbstractChangedFacilityService implements
-    ChangedEntityService<ChangedFacilityDto> {
+public abstract class AbstractChangedFacilityService
+    implements ChangedEntityService<ChangedFacilityDto> {
 
   private static final Logger LOG = LoggerFactory.getLogger(AbstractChangedFacilityService.class);
 
@@ -19,7 +20,11 @@ public abstract class AbstractChangedFacilityService implements
     String facilityId = null;
     try {
       facilityId = identifier.getId();
-      LOG.debug("Loading entity by id {}", facilityId);
+
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Loading entity by id {}", facilityId);
+      }
+
       FacilityDto facilityDto = loadEntityById(identifier);
       if (facilityDto == null) {
         LOG.error("Can't get facility by id {}", facilityId);
@@ -28,8 +33,7 @@ public abstract class AbstractChangedFacilityService implements
       return new ChangedFacilityDto(facilityDto, identifier.getRecordChangeOperation());
     } catch (Exception e) {
       LOG.error("Can't get facility by id {}", facilityId, e);
-      throw new IllegalStateException(
-          String.format("Can't get facility by id %s", facilityId), e);
+      throw new IllegalStateException(String.format("Can't get facility by id %s", facilityId), e);
     }
   }
 
