c2654293a7d36199a4c67e8ba48942fe2bb6ff4d
David R. Smith <David.Smith@osi.ca.gov>
2020-04-06 16:19:50 -0700

    CMO-475: discrepancy between CWS/CMS and CARES Facility Search

--- jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/QueryConstants.java
+++ jobs-facilities-cws/src/main/java/gov/ca/cwds/jobs/cals/facility/cws/QueryConstants.java
@@ -8,12 +8,8 @@ public final class QueryConstants {
   public static final String DATE_AFTER = "dateAfter";
   public static final String DATE_BEFORE = "dateBefore";
 
-  private static final String HOME_IDENTIFIER_FIELD_NAME = "home.identifier";
-  private static final String AND = " and ";
-  private static final String ORDER_BY = " order by ";
-
   // CMO-475: discrepancy between CWS/CMS and CARES Facility Search.
-  // This query ignores other affected tables other than PLC_HM_T.
+  // This query ignores tables related to PLC_HM_T.
   private static final String SHARED_PART = " from ReplicationPlacementHome as home"
       + " where home.licensrCd <> 'CL' and home.facilityType <> 1420"; // medical facility
 
@@ -30,17 +26,13 @@ public final class QueryConstants {
     public static final String GET_IDENTIFIERS_AFTER_TIMESTAMP_QUERY =
           "SELECT plh.IDENTIFIER, plh.IBMSNAP_OPERATION, plh.LST_UPD_TS AS IBMSNAP_LOGMARKER\n"
         + "FROM      {h-schema}PLC_HM_T plh\n"
-        + "LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
-        + "LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
         + "WHERE plh.LICENSR_CD <> 'CL'\n"
         + "  AND plh.PLC_FCLC   <> 1420\n"
         + "  AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
         + "  AND (\n"
         + "      plh.LST_UPD_TS > :dateAfter\n"
-        + "   OR cst.LST_UPD_TS > :dateAfter\n"
-        + "   OR stf.LST_UPD_TS > :dateAfter\n"
         + ")\n"
-        + "ORDER BY plh.LST_UPD_TS, cst.LST_UPD_TS, stf.LST_UPD_TS, plh.IDENTIFIER\n"
+        + "ORDER BY plh.LST_UPD_TS, plh.IDENTIFIER\n"
         + "FETCH FIRST BATCH_SIZE ROWS ONLY\n"
         + "FOR READ ONLY WITH UR";
     //@formatter:on
@@ -49,17 +41,13 @@ public final class QueryConstants {
     public static final String GET_IDENTIFIERS_BETWEEN_TIMESTAMPS_QUERY =
         "SELECT plh.IDENTIFIER, plh.IBMSNAP_OPERATION, plh.LST_UPD_TS AS IBMSNAP_LOGMARKER\n"
       + "FROM      {h-schema}PLC_HM_T plh\n"
-      + "LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
-      + "LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
       + "WHERE plh.LICENSR_CD <> 'CL'\n"
       + "  AND plh.PLC_FCLC   <> 1420\n"
       + "  AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
       + "  AND (\n"
       + "      plh.LST_UPD_TS BETWEEN :dateBefore AND :dateAfter\n"
-      + "   OR cst.LST_UPD_TS BETWEEN :dateBefore AND :dateAfter\n"
-      + "   OR stf.LST_UPD_TS BETWEEN :dateBefore AND :dateAfter\n"
       + ")\n"
-      + "ORDER BY plh.LST_UPD_TS, cst.LST_UPD_TS, stf.LST_UPD_TS, plh.IDENTIFIER\n"
+      + "ORDER BY plh.LST_UPD_TS, plh.IDENTIFIER\n"
       + "FETCH FIRST BATCH_SIZE ROWS ONLY\n"
       + "FOR READ ONLY WITH UR";
     //@formatter:on
@@ -69,29 +57,19 @@ public final class QueryConstants {
           "WITH STEP1 AS (\n"
         + " SELECT\n"
         + "   plh.LST_UPD_TS AS PLH_TS,\n"
-        + "   cst.LST_UPD_TS AS CST_TS,\n"
-        + "   stf.LST_UPD_TS AS STF_TS,\n"
         + "   plh.IDENTIFIER\n"
         + " FROM      {h-schema}PLC_HM_T plh\n"
-        + " LEFT JOIN {h-schema}CNTY_CST cst ON cst.IDENTIFIER = plh.FKCNTY_CST\n"
-        + " LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
         + " WHERE plh.LICENSR_CD <> 'CL'\n"
         + "   AND plh.PLC_FCLC   <> 1420\n"
         + "   AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
         + "   AND (\n"
         + "       plh.LST_UPD_TS > :dateAfter\n"
-        + "    OR cst.LST_UPD_TS > :dateAfter\n"
-        + "    OR stf.LST_UPD_TS > :dateAfter\n"
         + " )\n"
-        + " ORDER BY plh.LST_UPD_TS, cst.LST_UPD_TS, stf.LST_UPD_TS, plh.IDENTIFIER\n"
+        + " ORDER BY plh.LST_UPD_TS, plh.IDENTIFIER\n"
         + " FETCH FIRST BATCH_SIZE ROWS ONLY\n"
         + ")\n"
         + "SELECT MAX(x.LAST_TS) FROM (\n"
         + "  SELECT MAX(s1.PLH_TS) AS LAST_TS FROM STEP1 s1\n"
-        + "  UNION ALL\n"
-        + "  SELECT MAX(s1.CST_TS) AS LAST_TS FROM STEP1 s1 WHERE s1.CST_TS IS NOT NULL\n"
-        + "  UNION ALL\n"
-        + "  SELECT MAX(s1.STF_TS) AS LAST_TS FROM STEP1 s1 WHERE s1.STF_TS IS NOT NULL\n"
         + ") x\n"
         + "FOR READ ONLY WITH UR";
     //@formatter:on
@@ -108,15 +86,6 @@ public final class QueryConstants {
         + "       AND plh.IBMSNAP_OPERATION IN ('I','U')\n"
         + "       AND plh.LST_UPD_TS > :dateAfter\n"
         + " ) x\n"
-        + " UNION ALL\n"
-        + " SELECT cst.LST_UPD_TS AS IBMSNAP_LOGMARKER, cst.IBMSNAP_OPERATION\n"
-        + " FROM {h-schema}CNTY_CST cst\n"
-        + " WHERE cst.LST_UPD_TS > :dateAfter\n"
-        + " UNION ALL\n"
-        + " SELECT stf.LST_UPD_TS AS IBMSNAP_LOGMARKER, stf.IBMSNAP_OPERATION\n"
-        + " FROM      {h-schema}CNTY_CST cst\n"
-        + " LEFT JOIN {h-schema}STFPERST stf ON stf.IDENTIFIER = cst.FKSTFPERST\n"
-        + " WHERE stf.LST_UPD_TS > :dateAfter\n"
         + "), STEP2 AS (\n"
         + "  SELECT s1.IBMSNAP_LOGMARKER, s1.IBMSNAP_OPERATION\n"
         + "  FROM STEP1 s1\n"
