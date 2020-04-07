14848da09c8e13d6b9cfe5326886528abd4c7dff
David R. Smith <David.Smith@osi.ca.gov>
2020-04-07 14:39:11 -0700

    CMO-475: discrepancy between CWS/CMS and CARES Facility Search

--- jobs-facilities-cws/unix_cws_facility_index.sh
+++ jobs-facilities-cws/unix_cws_facility_index.sh
@@ -3,13 +3,9 @@
 # sudo gradle shadowJar
 ../gradlew shadowJar
 
-rm -rf cws-out
-mkdir cws-out
-
-# . /Users/dsmith/cws_legacy/project/cwds-jobs/classpath.sh
+# rm -rf cws-out
+# mkdir cws-out
 
 java -Dlog4j.configuration=file:log4j.properties -jar build/libs/cws-facilities-job-1.4-SNAPSHOT.jar \
      -c /Users/dsmith/ramdisk/hide/facilities-cws-job.yml -l ./cws-out/ 
-
-#\
 # > ./cws-out/out_Facility.txt 2>&1
